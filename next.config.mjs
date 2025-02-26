f16cb1ac863d8ce6d043fbeeb06b60d82dcae2e4
Rachit Trivedi <114496132+technoprogramming92@users.noreply.github.com>
2025-02-26 17:21:40 +0530

    changes-26225-17:19-rht

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-import { promises as fs } from "fs";
-import path from "path";
-
-const nextConfig = {
-  webpack: (config) => {
-    config.plugins.push({
-      apply: (compiler) => {
-        compiler.hooks.done.tapPromise("CopyPublicJs", async () => {
-          const sourceDir = path.resolve("public/js");
-          const destDir = path.resolve(".next/static/js");
-
-          try {
-            await fs.mkdir(destDir, { recursive: true });
-
-            const files = await fs.readdir(sourceDir);
-            await Promise.all(
-              files.map((file) =>
-                fs.copyFile(`${sourceDir}/${file}`, `${destDir}/${file}`)
-              )
-            );
-          } catch (err) {
-            console.error("Error copying JS files:", err);
-          }
-        });
-      },
-    });
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
